79af438823bebfe629d5e8f6df8dbf72ec21987e
Sweety <sweety.webashlar@gmail.com>
2019-06-10 09:40:14 +0530

    switch all select functionality

--- src/app/shared/toggle-switch/toggle-switch.component.ts
+++ src/app/shared/toggle-switch/toggle-switch.component.ts
@@ -9,7 +9,27 @@ import * as uuid from 'uuid';
 export class ToggleSwitchComponent {
   @Input() size = 'medium';
   @Input() theme = 'dark';
+  @Input() type: string;
+  @Input() key: string;
+
+  @Input() dataArr: any = [];
+  @Output() dataArrChange: EventEmitter<boolean> = new EventEmitter<any>();
+
   @Input() toggled: boolean;
   @Output() toggledChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+
   inputId = uuid.v4();
+
+  checkAll(event?) {
+    this.dataArr.forEach(x => x[this.key] = event.target.checked);
+    this.toggled = event.target.checked;
+    this.toggledChange.emit(this.toggled);
+    this.dataArrChange.emit(this.dataArr);
+  }
+
+  isAllChecked() {
+    const isTrue = this.dataArr.every(o => o[this.key]);
+    this.dataArrChange.emit(this.dataArr);
+    return isTrue;
+  }
 }
