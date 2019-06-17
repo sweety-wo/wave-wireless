248c584e83cf0942e5938a1d7ea00f574f9e8363
sweety-wo <sweety.webashlar@gmail.com>
2019-06-17 19:22:08 +0530

    - Added support for websocket

--- src/app/pages/main-container/map/map.component.ts
+++ src/app/pages/main-container/map/map.component.ts
@@ -1,26 +1,31 @@
-import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
 import 'leaflet.markercluster';
 import {Constant} from '../../../constant/constant';
 import {OpenStreetMapProvider} from 'leaflet-geosearch';
 import * as _ from 'lodash';
+import {Paho} from 'ng2-mqtt/mqttws31';
 import {DeviceService} from '../../../services/node/device.service';
+import {CommonService} from '../../../services/common/common.service';
+import {AuthService} from '../../../services/auth-service/auth.service';
 
 @Component({
     selector: 'app-map',
     templateUrl: './map.component.html',
     styleUrls: ['./map.component.scss']
 })
-export class MapComponent implements OnInit {
-
+export class MapComponent implements OnInit, OnDestroy {
     deviceData: any;
     filteredData: any;
     centerLat: any;
     centerLong: any;
     geoResult: any;
     isDeviceLoading: boolean;
+    private _client: Paho.MQTT.Client;
     isGeoSearch: boolean;
 
-    constructor(private _device: DeviceService, private ref: ChangeDetectorRef) {
+    constructor(private _device: DeviceService,
+                private _common: CommonService,
+                private _auth: AuthService) {
         this.deviceData = Constant.deviceData;
         this.isDeviceLoading = true;
         this.centerLat = 38.89511;
@@ -33,7 +38,14 @@ export class MapComponent implements OnInit {
         this.filteredData = _.filter(this.deviceData, (device: any) => {
             return (device.health === Constant.CRITICAL_HEALTH || device.health === Constant.ATTENTION_HEALTH);
         });
-       this.setLatLng(this.filteredData);
+        this.setLatLng(this.filteredData);
+        this.fnInitializeWebSockets();
+    }
+
+    ngOnDestroy() {
+        if (this._client) {
+            this._client.disconnect();
+        }
     }
 
     setLatLng(data) {
@@ -53,16 +65,83 @@ export class MapComponent implements OnInit {
         }
     }
 
-    getDevices() {
+    getDevices () {
         this.isDeviceLoading = false;
         /*this._device.getDevices().subscribe((devices) => {
-            console.log('devices', devices);
             this.deviceData = devices;
             this.filteredData = devices;
             this.isDeviceLoading = false;
         }, (err) => {
             this.isDeviceLoading = false;
         });*/
+    }
+
+    onConnectionLost (responseObject) {
+        if (responseObject.errorCode !== 0) {
+            this.fnInitializeWebSockets();
+        }
+    }
+
+    onMessageArrived (message) {
+        const messageJson = JSON.parse(message.payloadString);
+        const index = _.findIndex(this.deviceData, (device: any) => {
+            return device.id === messageJson.id;
+        });
+        if (index !== -1) {
+            this.fnUpdateDevices(messageJson, index, this.deviceData);
+        }
+        const filteredIndex = _.findIndex(this.filteredData, (device: any) => {
+            return device.id === messageJson.id;
+        });
+        if (filteredIndex !== -1) {
+            this.fnUpdateDevices(messageJson, index, this.filteredData);
+        }
+    }
+
+    fnUpdateDevices(messageJson, index, devicesArr) {
+        if (devicesArr[index]) {
+            if (messageJson.health) {
+                devicesArr[index].health = messageJson.health;
+            }
+            if (messageJson.phase) {
+                devicesArr[index].phase = messageJson.phase;
+            }
+            if (messageJson.connection) {
+                devicesArr[index].connection = messageJson.connection;
+            }
+        }
+    }
+
+    fnInitializeWebSockets () {
+        let loginUser: any;
+        const self = this;
+        this._auth.loggedInUser.subscribe((user) => {
+            if (user) {
+                loginUser = user;
+                const webSocketId = loginUser.id.split('-')[0];
+                this._common.getWebSocketUrl('wss/node/devices/' + webSocketId)
+                    .subscribe((res: any) => {
+                        self._client = new Paho.MQTT.Client(res.url, res.clientId);
+
+                        self._client.onConnectionLost = self.onConnectionLost;
+                        self._client.onMessageArrived = self.onMessageArrived;
+
+                        const connectOptions = {
+                            onSuccess: function () {
+                                // console.log('device websocket onConnect'); // eslint-disable-line
+                                self._client.subscribe(res.topic, '');
+                            },
+                            useSSL: true,
+                            timeout: 3,
+                            mqttVersion: 4,
+                            onFailure: function () {
+                                /*console.log("device websocket onFail"); //eslint-disable-line*/
+                            }
+                        };
+                        self._client.connect(connectOptions);
+                    });
+            }
+        });
     }
 
     async fnSearch(res: any) {
